ddb6c707a01feed59c9da8cc48d12464ad9e208a
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2021-09-30 15:06:31 +0800

    Update tooltip.js

--- js/tooltip.js
+++ js/tooltip.js
@@ -15,4 +15,4 @@
  */
 'use strict';
 
-scene.hotspotContainer().createHotspot(document.querySelector("#tooltip"), { yaw: 2.95, pitch: -0.05 });
+scene.hotspotContainer().createHotspot(document.querySelector("#tooltip"), { yaw: -0.07959458102301298, pitch: -0.22365486730027584 });
